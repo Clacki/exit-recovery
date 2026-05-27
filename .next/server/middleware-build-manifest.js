2f0de2364a25e7e185da60d08581734cf6478fd7
Clacki <skthk6613@naver.com>
2026-05-27 21:54:38 +0900

    chore: 빌드 산출물 제거 및 gitignore 정리

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,23 +1,27 @@
 globalThis.__BUILD_MANIFEST = {
   "pages": {
-    "/_app": []
+    "/_app": [
+      "static/chunks/6491ac80415832a1.js",
+      "static/chunks/d501c0e7777b1e1c.js",
+      "static/chunks/turbopack-117906deff2d3aa7.js"
+    ],
+    "/_error": [
+      "static/chunks/da4163fe9b95076a.js",
+      "static/chunks/d501c0e7777b1e1c.js",
+      "static/chunks/turbopack-1412dba71f053906.js"
+    ]
   },
   "devFiles": [],
   "ampDevFiles": [],
   "polyfillFiles": [
-    "static/chunks/node_modules_next_dist_build_polyfills_polyfill-nomodule.js"
+    "static/chunks/a6dad97d9634a72d.js"
   ],
   "lowPriorityFiles": [],
   "rootMainFiles": [
-    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_57d40746._.js",
-    "static/chunks/node_modules_next_dist_compiled_react-dom_1e674e59._.js",
-    "static/chunks/node_modules_next_dist_compiled_next-devtools_index_a9cb0712.js",
-    "static/chunks/node_modules_next_dist_compiled_5150ccfd._.js",
-    "static/chunks/node_modules_next_dist_client_cf1d9188._.js",
-    "static/chunks/node_modules_next_dist_b0daae9a._.js",
-    "static/chunks/node_modules_@swc_helpers_cjs_b3dc30d6._.js",
-    "static/chunks/_a0ff3932._.js",
-    "static/chunks/turbopack-_cdba956c._.js"
+    "static/chunks/c85a7d07e7b82c9a.js",
+    "static/chunks/0ed411adebc7f91d.js",
+    "static/chunks/bae01d964e8808cf.js",
+    "static/chunks/turbopack-6f87c88223edc345.js"
   ],
   "ampFirstPages": []
 };
--- .next/static/chunks/src_app_layout_tsx_0a548d63._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_globals_91e4631d.css",
-  "static/chunks/node_modules_next_dist_b47712da._.js",
-  "static/chunks/src_app_providers_tsx_9c18dcf0._.js"
-],
-    source: "dynamic"
-});
